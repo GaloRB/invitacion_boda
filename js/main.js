19c0407b090d11d96578c5222bcbb68263bc7d14
Galo Barcenas <galobarcenas@192.168.1.7>
2024-12-11 20:02:04 -0600

    con musica2

--- js/main.js
+++ js/main.js
@@ -228,6 +228,14 @@
 	// audio
 	let sound = document.querySelector('#sound');
 
+	function play_single_soung(){
+		window.onload=function(){
+			document.querySelector('#clic').click();
+		}
+	}
+
+	window.onbeforeunload = play_single_soung();
+
 	window.addEventListener('scroll', ()=>{
 		sound.play();
 	})
